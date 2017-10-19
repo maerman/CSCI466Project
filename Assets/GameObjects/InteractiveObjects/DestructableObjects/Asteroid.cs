d048dbb03f934f0f9557c877c260bb045077ed11
Unknown <tls5200@gmail.com>
2017-10-18 20:59:59 -0400

    Added Colors, Fixed Bugs
    Made a white version of Sphere.png so it could have color added to it. Changed the colors of LazerShot, Blob and second Player. 
    
    Also fixed a bug in Asteriod.

--- Assets/GameObjects/InteractiveObjects/DestructableObjects/Asteroid.cs
+++ Assets/GameObjects/InteractiveObjects/DestructableObjects/Asteroid.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Asteroid :DestructableObject
 {
+    public float minSize = 0.3f;
+
     protected override void destructableObjectCollision(DestructableObject other, Collision2D collision)
     {
 
@@ -35,27 +37,42 @@ public class Asteroid :DestructableObject
 
     public override void damageThis(float damage)
     {
-        int pieces = (int)(damage * 8 / health);
-
-        base.damageThis(damage);
-
-        float area = (float)(scale.x * scale.x / 4 * System.Math.PI);
-        area /= pieces;
-        float theScale = (float)(System.Math.Sqrt(area / System.Math.PI * 4));
-
-        for (int i = 0; i < pieces; i++)
+        if (damage - armor > 0)
         {
-            float theAngle = i * 360.0f / pieces + angle;
-            Asteroid current = (Asteroid)level.createObject("AsteroidPF", position + new Vector2(1, 0).rotate(theAngle), angle,
-                velocity + new Vector2(pieces, 0).rotate(theAngle), angularVelocity + pieces, theScale);
+            if (damage - armor > health)
+            {
+                destroyThis();
+            }
+            else if (scale.x < minSize)
+            {
+                base.damageThis(damage);
+            }
+            else
+            {
+                int pieces = (int)(damage * 8 / health);
 
-            current.mass = mass / pieces;
-            current.health = health / pieces;
+                base.damageThis(damage);
+
+                float area = (float)(scale.x * scale.x / 4 * System.Math.PI);
+                area /= pieces;
+                float theScale = (float)(System.Math.Sqrt(area / System.Math.PI * 4));
+
+                for (int i = 0; i < pieces; i++)
+                {
+                    float theAngle = i * 360.0f / pieces + angle;
+                    Asteroid current = (Asteroid)level.createObject("AsteroidPF", position + new Vector2(1, 0).rotate(theAngle), angle,
+                        velocity + new Vector2(pieces, 0).rotate(theAngle), angularVelocity + pieces, theScale);
+
+                    current.mass = mass / pieces;
+                    current.health = health / pieces;
+                }
+
+                if (pieces > 0)
+                {
+                    destroyThis();
+                }
+            }
         }
 
-        if (pieces > 0)
-        {
-            destroyThis();
-        }
     }
 }
