2638ab0d1afc96061837aac03f99a960d8b00c03
maerman <maerman@mail.fhsu.edu>
2017-10-21 18:39:16 -0400

    Adding needed untracked untracked files

---
+++ Assets/Levels/Level3.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using static User;
+using static CRUD;
+using static GameStates;
+
+public class Level3 : Level
+{
+    public override int levelNumber
+    {
+        get
+        {
+            return 3;
+        }
+    }
+
+    protected override void initilizeLevel()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+             HomingMine current = (HomingMine)createObject("HomingMinePF", getRandomPosition(), getRandomAngle(), getRandomVelocity(10), random.Next(100));
+        }
+    }
+
+    protected override void updateLevel()
+    {
+        
+    }
+}
---
+++ Assets/Levels/Level4.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using static User;
+using static CRUD;
+using static GameStates;
+
+public class Level4 : Level
+{
+    public override int levelNumber
+    {
+        get
+        {
+            return 4;
+        }
+    }
+
+    protected override void initilizeLevel()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+             HomingMine current = (HomingMine)createObject("HomingMinePF", getRandomPosition(), getRandomAngle(), getRandomVelocity(10), random.Next(100));
+        }
+    }
+
+    protected override void updateLevel()
+    {
+        
+    }
+}
